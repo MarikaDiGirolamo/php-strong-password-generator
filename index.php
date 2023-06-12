adfa5adba5f6414111a2ae705769ba258db9ba3e
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-06-12 08:17:48 +0200

    layouting

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,10 +11,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/style.css">
     <title>php-strong-password-generator</title>
 </head>
 
 <body>
+    <div class="container">
+
+        <h1>STRONG GENERATION PASSWORD</h1>
+        <h3>Generate a secure password</h3>
+
+        <div class="container form">
+            <form action="index.php" method="GET">
+                <label for="pwd">Lunghezza password:</label>
+                <input type="text" name="pwd" value="">
+            </form>
+            <div class="container btn-container">
+                <button class="btn btn-outline-primary" type="submit">Submit</button>
+                <button class="btn btn-outline-secondary" type="reset">Reset</button>
+
+            </div>
+
+        </div>
+    </div>
 
 </body>
 
